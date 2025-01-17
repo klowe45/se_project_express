47c86c7e2ff4c03ddb94c157a08fb015ca6b7126
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-01-17 08:56:15 -0600

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -13,6 +14,8 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", mainRouter);
 
